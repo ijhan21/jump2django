e83e047f05f786b674c038a5aab4c195aee0644f
Hans <reinforcehan@gmail.com>
2022-09-15 16:59:35 +0900

    processing

--- pybo/views.py
+++ pybo/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Question
 # Create your views here.
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    # return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    question_list = Question.objects.order_by('-create_date') # 앞에 - 때문에 역순 정렬
+    context = {'question_list':question_list}
+    # return HttpResponse("hello")
+    return render(request, 'pybo/question_list.html', context)
