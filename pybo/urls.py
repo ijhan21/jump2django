f511b53b218fc2ee5ec77ebfbc54ebca8d973284
Hans <reinforcehan@gmail.com>
2022-09-19 13:08:29 +0900

    gitignore

--- pybo/urls.py
+++ pybo/urls.py
@@ -3,5 +3,8 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('', views.index),
+    # path('', views.index),
+    # path('<int:question_id>', views.detail),
+    path('', views.IndexView.as_view()),
+    path('<int:pk>/', views.DetailView.as_view())
 ]
--- pybo/views.py
+++ pybo/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from .models import Question
+from django.views import generic
 # Create your views here.
 def index(request):
     # return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
@@ -8,3 +9,16 @@ def index(request):
     context = {'question_list':question_list}
     # return HttpResponse("hello")
     return render(request, 'pybo/question_list.html', context)
+
+def detail(request, question_id):
+    # question = Question.objects.get(id=question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    context = {'question':question}
+    return render(request, 'pybo/question_detail.html', context)
+
+class IndexView(generic.ListView):
+    def get_queryset(self):
+        return Question.objects.order_by('-create_date')
+
+class DetailView(generic.DetailView):
+    model = Question
